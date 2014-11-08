459bf91c7f79a9b3e80915e5a1dea5d01857780d
ButenkoT <tanya@butenko.me>
2014-11-08 19:51:24 +1100

    Fix flight-user-reservation dependencies.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -15,5 +15,5 @@
 class Flight < ActiveRecord::Base
   belongs_to :airplane
   has_many :reservations
-  has_and_belongs_to_many :users, :through => :reservations
+  has_many :users, :through => :reservations
 end
